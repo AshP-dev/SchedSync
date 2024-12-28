2e78f03f88a32ec34488a3dca96ee2a3ed27d6a7
AshP-dev <ashwatthap@gmail.com>
2024-12-28 09:44:18 +0530

    Added review buttons on the frontend; need to modify the interface and backend so that the values can be stored and used for spaced repitition.

--- frontend/src/Card.js
+++ frontend/src/Card.js
@@ -8,6 +8,22 @@ const Card = ({ card }) => {
     setFlipped(!flipped);
   };
 
+  const handleReview = (rating) => {
+    fetch(`http://localhost:3010/api/cards/${card.id}/review`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ rating }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Review submitted:", data);
+      })
+      .catch((error) => {
+        console.error("Error submitting review:", error);
+      });
+  };
   return (
     <div className={`card ${flipped ? "flipped" : ""}`} onClick={handleFlip}>
       <div className='card-inner'>
@@ -23,6 +39,14 @@ const Card = ({ card }) => {
               More Info
             </a>
           )}
+          <div className='review-buttons'>
+            <p>How likely are you to make this again?</p>
+            <button onClick={() => handleReview(1)}>1</button>
+            <button onClick={() => handleReview(2)}>2</button>
+            <button onClick={() => handleReview(3)}>3</button>
+            <button onClick={() => handleReview(4)}>4</button>
+            <button onClick={() => handleReview(5)}>5</button>
+          </div>
         </div>
       </div>
     </div>
